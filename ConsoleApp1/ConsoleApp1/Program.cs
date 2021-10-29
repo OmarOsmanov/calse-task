484c727598f4a23783034d9bd12d59aa4b2948ff
OmarOsmanov <92145550+OmarOsmanov@users.noreply.github.com>
2021-10-29 10:53:23 +0400

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -47,3 +47,71 @@ namespace ConsoleApp1
         }
     }
 }
+
+//tamamile 1 ci task
+using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task 1
+            {
+
+
+                home home1 = new home();
+                home1.otaq = 3;
+                home1.mertebe = 5;
+                home1.unvan = "sabit rehman";
+                home1.telefon = "0554034430";
+                home1.seher = "baki";
+
+                home home2 = new home();
+                home2.otaq = 5;
+                home2.mertebe = 2;
+                home2.unvan = "sabit rehman";
+                home2.telefon = "0559034430";
+                home2.seher = "seki";
+
+                Console.WriteLine(home1.homes());
+
+                home home3 = new home();
+                home3.otaq = 9;
+                home3.mertebe = 7;
+                home3.unvan = "sabit rehman";
+                home3.telefon = "0559035330";
+                home3.seher = "seki";
+
+
+
+            }
+
+        }
+
+        class home
+        {
+            public byte otaq;
+            public int mertebe;
+            public string unvan;
+            public string telefon;
+            public string seher;
+
+            public string homes()
+            {
+                return ($" Unvan budur { unvan}  telefon nomresi { telefon}  yasadigi seher { seher} ");
+            }
+
+
+            #endregion
+
+
+
+
+
+
+        }
+          }
+    }   
